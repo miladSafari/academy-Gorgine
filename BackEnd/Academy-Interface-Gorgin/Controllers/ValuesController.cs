b521ae1a080c20f8d90e5213808ffd474640b0fe
Mr.Dragon <Mr.Dragon@DESKTOP-M7N8K0R>
2019-04-03 14:19:54 +0430

    Eddit Name Space

--- BackEnd/Academy-Interface-Gorgin/Controllers/ValuesController.cs
+++ BackEnd/Academy-Interface-Gorgin/Controllers/ValuesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Academy_Interface_Gorgin.Controllers
+namespace Academy.Interface.Gorgin.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- BackEnd/Academy-Interface-Gorgin/Startup.cs
+++ BackEnd/Academy-Interface-Gorgin/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Academy_Interface_Gorgin
+namespace Academy.Interface.Gorgin
 {
     public class Startup
     {
