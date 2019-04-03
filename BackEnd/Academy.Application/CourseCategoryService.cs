8e6756a4ed4e5e14349d3bab49b9dd0dbdcabd06
Mr.Dragon <Mr.Dragon@DESKTOP-M7N8K0R>
2019-04-03 14:17:20 +0430

    create CourseCategory Model and Service

---
+++ BackEnd/Academy.Application/CourseCategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Academy.Application
+{
+    public class CourseCategoryService
+    {
+        public void CreateCourseCategory()
+        {
+            var courseCategory=new CourseCategoryService();
+
+            // Save CourseCategory in DB
+        }
+    }
+}
---
+++ BackEnd/Academy.Domain/Model/CourseCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Academy.Domain.Model
+{
+    public class CourseCategory
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public CourseCategory Parent { get; set; }
+    }
+}
